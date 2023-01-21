dd91727710d6f9866e499760d109a70d16f25aee
sharmeen <sharmeen1209@gmail.com>
2023-01-21 19:36:52 +0530

    content added

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from plyer import notification
 if __name__ == '__main__':
 	while True:
 		notification.notify(
-			title = "Title",
-			message ="Description",
+			title = "Daily Reminder",
+			message ="Amazing things will happen to you!",
 			timeout= 12
 		)
+		# delay for 24 hours
 		time.sleep(60*60)
