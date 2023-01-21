454dee69f879e112937f7673c9b13d7bd07e6ce9
sharmeen <sharmeen1209@gmail.com>
2023-01-21 19:53:22 +0530

    icon added

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ if __name__ == '__main__':
 	while True:
 		notification.notify(
 			title = "Daily Reminder",
-			message ="Amazing things will happen to you!",
-			timeout= 12
+			message = "Amazing things will happen to you!",
+			app_icon = "icon.ico",
+			timeout = 12
 		)
 		# delay for 24 hours
 		time.sleep(60*60)
